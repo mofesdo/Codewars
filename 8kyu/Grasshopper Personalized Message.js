ed353e74dbcffc112631a20c3cbab3dea0f268ad
Modesto Amador <mofesdo.dev@gmail.com>
2024-01-04 16:18:48 -0800

    personalized message added

---
+++ 8kyu/Grasshopper Personalized Message.js
@@ -0,0 +1,10 @@
+// Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+function greet (name, owner) {
+    // Add code here
+    if(name === owner){
+      return "Hello boss"
+    }
+    else{
+      return "Hello guest"
+    }
+  }
