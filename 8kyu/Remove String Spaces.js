aed9fef978d8111acfa3d51027c71114fd7dbc9d
Modesto Amador <mofesdo.dev@gmail.com>
2024-01-16 21:58:59 -0800

    remove string spaces done

---
+++ 8kyu/Remove String Spaces.js
@@ -0,0 +1,10 @@
+// Write a function that removes the spaces from the string, then return the resultant string.
+function noSpace(x){
+    let result = ""
+    for(let i = 0; i < x.length; i++){
+      if(x[i] != " "){
+        result = result + x[i]
+      }
+    }
+    return result;
+  }
