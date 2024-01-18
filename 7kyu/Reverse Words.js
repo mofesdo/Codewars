265621841d9ce1a6649ea7e4885be00ee0e62556
Modesto Amador <mofesdo.dev@gmail.com>
2024-01-17 22:14:01 -0800

    rev string done

--- 7kyu/Reverse Words.js
+++ 7kyu/Reverse Words.js
@@ -1,1 +1,19 @@
 //Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+function reverseWords(str) {
+    // Go for it
+    let res = ""
+    let revStr = ""
+    for(let i = str.length - 1; i >= 0; i--){
+      res = res + str[i]
+    }
+    let resArr = res.split(" ");
+    console.log(resArr)
+    for(let i = resArr.length - 1; i >= 0; i--){
+      revStr = revStr + resArr[i]
+      if(i != 0){
+        revStr = revStr + " "
+      }
+    }
+    
+    return revStr
+  }
