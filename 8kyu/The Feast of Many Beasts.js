7cde2725b1bd758c042ccc2fe83403ca82b80a68
Modesto Amador <mofesdo.dev@gmail.com>
2024-02-08 23:23:24 -0800

    the feast of many beasts done

--- 8kyu/The Feast of Many Beasts.js
+++ 8kyu/The Feast of Many Beasts.js
@@ -4,3 +4,10 @@ Write a function feast that takes the animal's name and dish as arguments and re
 
 Assume that beast and dish are always lowercase strings, and that each has at least two letters. beast and dish may contain hyphens and spaces, but these will not appear at the beginning or end of the string. They will not contain numerals.
 */
+function feast(beast, dish) {
+    //your function here
+      if(beast[0] == dish[0] && beast[beast.length-1] == dish[dish.length-1]){
+        return true
+      }
+      return false
+    }
