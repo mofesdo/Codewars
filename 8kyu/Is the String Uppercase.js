291f534c12bbabc731c35c0b01f9cdbfaa8e0533
Modesto Amador <mofesdo.dev@gmail.com>
2024-02-19 23:45:55 -0800

    finished kata. referenced string argument by using "this"

--- 8kyu/Is the String Uppercase.js
+++ 8kyu/Is the String Uppercase.js
@@ -1,5 +1,7 @@
 //Create a method to see whether the string is ALL CAPS.
 String.prototype.isUpperCase = function() {
     // your code here
-    
+    console.log(this)
+    console.log(this.toUpperCase())
+    return this.toUpperCase() == this ? true : false 
   }
