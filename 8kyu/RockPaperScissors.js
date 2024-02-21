ec4dda29d72042b649e25e07563a833b732b24c6
Modesto Amador <mofesdo.dev@gmail.com>
2024-02-20 23:51:59 -0800

    done

--- 8kyu/RockPaperScissors.js
+++ 8kyu/RockPaperScissors.js
@@ -1,5 +1,13 @@
 // Let's play! You have to return which player won! In case of a draw return Draw!
 
 const rps = (p1, p2) => {
-  
-};
+    if(p1 == p2){
+      return "Draw!"
+    }
+    else if((p1 == "rock" && p2 == "scissors") || (p1 == "paper" && p2 == "rock") || (p1 == "scissors" && p2 == "paper")){
+      return "Player 1 won!"
+    }
+    else{
+      return "Player 2 won!"
+    }
+  };
