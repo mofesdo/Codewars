3c4be01b0dfb33ae4c2b0a7bd10ad0db49dad014
Modesto Amador <mofesdo.dev@gmail.com>
2024-03-01 23:43:14 -0800

    fixed round to the next 5

--- 7kyu/Round up to the next multiple of 5.js
+++ 7kyu/Round up to the next multiple of 5.js
@@ -1,10 +1,14 @@
 /* Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
 */
 function roundToNext5(n){
-    // ...
-    if(n%5 != 0){
-      if(n >= 0){
-       return n - n%5 + 5 
-      }
-      else{
-        r
+  // ...
+  if(n%5 != 0){
+    if(n >= 0){
+     return n - n%5 + 5 
+    }
+    else{
+      return n - n%5 
+    }
+  }
+  return n
+}
