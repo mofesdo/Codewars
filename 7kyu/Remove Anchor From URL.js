955486d32c4c889e4353f73d4927bae7699b94eb
Modesto Amador <mofesdo.dev@gmail.com>
2024-03-04 23:39:32 -0800

    kata complete

--- 7kyu/Remove Anchor From URL.js
+++ 7kyu/Remove Anchor From URL.js
@@ -1,4 +1,10 @@
 //Complete the function/method so that it returns the url with anything after the anchor (#) removed.
 function removeUrlAnchor(url){
     // TODO: complete
+    for(let i = 0; i < url.length; i++){
+      if(url[i] == "#"){
+        return url.slice(0,i)
+      }
+    }
+    return url
   }
