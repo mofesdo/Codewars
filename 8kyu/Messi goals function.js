47ca19b86b8e349789644940fce0eb25f243d707
Modesto Amador <mofesdo.dev@gmail.com>
2024-03-07 22:28:29 -0800

    messi goals done

---
+++ 8kyu/Messi goals function.js
@@ -0,0 +1,5 @@
+//Complete the function to return his total number of goals in all three leagues.
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+    // code goes here
+    return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+  }
