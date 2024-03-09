fefd0339960142bb3b005ca9c541ee0b517a05b3
Modesto Amador <mofesdo.dev@gmail.com>
2024-03-08 23:55:04 -0800

    getting closer failed 1 test

--- 7kyu/Sort array by string length.js
+++ 7kyu/Sort array by string length.js
@@ -2,13 +2,15 @@
 function sortByLength (array) {
     // Return an array containing the same strings,
     // ordered from shortest to longest
-    for(let i = 0; i < array.length - 1; i++){
-            console.log(array)
-      if(array[i].length > array[i+1].length){
-        let temp = array[i+1];
-        array[i+1] = array[i]
-        array[i] = temp;
+    for(let i = 0; i < array.length ; i++){
+      for(let j = 0; j < array.length ; j++){
+        if(array[i].length < array[j].length){
+          let temp = array[j];
+          array[j] = array[i]
+          array[i] = temp;
+        }
       }
+            console.log(array)
     }
       return array;
   }
