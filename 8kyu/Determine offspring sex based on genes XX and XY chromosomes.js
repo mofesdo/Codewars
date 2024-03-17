a00ef2b405f43c75701b8339eed3566c6f56cda8
Modesto Amador <mofesdo.dev@gmail.com>
2024-03-17 15:08:40 -0700

    determine sex of offspring kata done

---
+++ 8kyu/Determine offspring sex based on genes XX and XY chromosomes.js
@@ -0,0 +1,4 @@
+// Determine if the sex of the offspring will be male or female based on the X or Y chromosome present in the male's sperm.
+function chromosomeCheck(sperm) {
+    return sperm == "XY" ? "Congratulations! You're going to have a son." : "Congratulations! You're going to have a daughter."
+  }
