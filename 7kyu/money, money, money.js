d547ecd26433dcf7602e6e82d10e6d3bc3b04cda
Modesto Amador <mofesdo.dev@gmail.com>
2024-04-01 23:59:32 -0700

    kata done

--- 7kyu/money, money, money.js
+++ 7kyu/money, money, money.js
@@ -1,13 +1,11 @@
-
 function calculateYears(principal, interest, tax, desired) {
     // your code
   let count = 0;
   let accrued = 0;
   while(principal < desired){
-    principal = principal + ((principal * interest)-(pr);
-    
-    
+    principal = principal + ((principal * interest)-(principal * interest * tax));
     count++;
     console.log(principal)
   }
+  return count;
 }
