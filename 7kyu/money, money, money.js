3b8b8059d5483e8b91513c6872a7b5bd119e26ec
Modesto Amador <mofesdo.dev@gmail.com>
2024-04-01 23:55:18 -0700

    emergency puysh

--- 7kyu/money, money, money.js
+++ 7kyu/money, money, money.js
@@ -1,4 +1,13 @@
 
 function calculateYears(principal, interest, tax, desired) {
     // your code
+  let count = 0;
+  let accrued = 0;
+  while(principal < desired){
+    principal = principal + ((principal * interest)-(pr);
+    
+    
+    count++;
+    console.log(principal)
+  }
 }
