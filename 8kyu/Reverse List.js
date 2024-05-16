2336c7ce79d11798bf42ee06d7ff0b521f481404
Modesto Amador <mofesdo.dev@gmail.com>
2024-05-15 18:33:55 -0700

    reverse list kata done

--- 8kyu/Reverse List.js
+++ 8kyu/Reverse List.js
@@ -6,5 +6,11 @@ Examples (Input -> Output)
 */
 //emergency push
 function reverseList(list) {
-
-}
+    let newList = []
+    let count = 0
+    for(let i = list.length -1; i >= 0; i--){
+      newList[count] = list[i]
+      count++
+    }
+    return newList;
+  }
