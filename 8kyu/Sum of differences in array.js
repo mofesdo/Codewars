f134eab64d968151d87007cd5d8b3922e90dd0a7
Modesto <mofesdo.dev@gmail.com>
2024-09-08 18:17:40 -0700

    kata done

--- 8kyu/Sum of differences in array.js
+++ 8kyu/Sum of differences in array.js
@@ -8,3 +8,11 @@ Sum: (10 - 2) + (2 - 1) = 8 + 1 = 9
 
 If the array is empty or the array has only one element the result should be 0 (Nothing in Haskell, None in Rust).
 */
+function sumOfDifferences(arr) {
+    arr.sort((a, b) => a - b);
+    let b = 0;
+    for(let a = arr.length - 1; a > 0; a--) {
+      b += arr[a] - arr[a - 1];
+    }
+    return b;
+  }
