7fe05d5d16447c063beb164a44184c5744c4e434
Modesto <mofesdo.dev@gmail.com>
2024-09-20 19:28:44 -0700

    ball kata done

---
+++ 8kyu/Regular Ball Super Ball.js
@@ -0,0 +1,21 @@
+/*Create a class Ball. Ball objects should accept one argument for "ball type" when instantiated.
+
+If no arguments are given, ball objects should instantiate with a "ball type" of "regular."
+
+ball1 = new Ball();
+ball2 = new Ball("super");
+
+ball1.ballType     //=> "regular"
+ball2.ballType     //=> "super"
+*/
+
+var Ball = function(ballType) {
+    class Ball {
+      constructor(ballType) {
+        this.ballType = ballType || "regular";
+      }
+    }
+    
+      let ball1 = new Ball(ballType);
+      return ball1;
+    };
