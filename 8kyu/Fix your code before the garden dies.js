2d1208a95bbffd41d08c69154af2ac1fdcf297c2
Modesto <mofesdo.dev@gmail.com>
2024-10-31 23:15:08 -0700

    Completed: Fix your code before the garden dies

--- 8kyu/Fix your code before the garden dies.js
+++ 8kyu/Fix your code before the garden dies.js
@@ -4,10 +4,10 @@ Your task is to debug the code before your plants die!
 */
 
 function rainAmount(mm){
-    if (rainAmount == 40) {
-         return `You need to give your plant ${rainAmount - 40} mm of water`
+    if (mm < 40) {
+      return `You need to give your plant ${40 - mm}mm of water`;
     }
     else {
-         return "Your plant has had more than enough water for today!"
-    };
+      return "Your plant has had more than enough water for today!";
+    }
 }
