978c2ebf45bb6f112be835ec3ae83428e071f24d
Modesto <mofesdo.dev@gmail.com>
2024-11-11 22:44:00 -0800

    completed: predict your age kata

--- 7kyu/Predict your age!.js
+++ 7kyu/Predict your age!.js
@@ -11,5 +11,7 @@ Example
 predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
 */
 function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
-    // your code
-  }
+  // your code
+  let result = Math.sqrt((age1 * age1) + (age2 * age2) + (age3 * age3) + (age4 * age4) + (age5 * age5) + (age6 * age6) + (age7 * age7) + (age8 * age8))/2;
+  return Math.floor(result);
+}
