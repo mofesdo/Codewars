1548038f92f169becfaeb425907f60cfb6ff219e
Modesto Amador <49541900+mofesdo@users.noreply.github.com>
2025-06-27 23:13:21 -0700

    Add solution - CodeHub

---
+++ 8-kyu/color-ghost/color-ghost.js
@@ -0,0 +1,6 @@
+let Ghost = function() {
+  // your code goes here
+    const colors = ["white", "yellow", "purple", "red"];
+    this.color = colors[Math.floor(Math.random() * colors.length)];
+  
+};
