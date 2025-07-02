eee345895be066aa88a9816da3bbd28f4002a46b
Modesto Amador <49541900+mofesdo@users.noreply.github.com>
2025-07-01 17:18:33 -0700

    Add solution - CodeHub

---
+++ 7-kyu/building-blocks/building-blocks.js
@@ -0,0 +1,26 @@
+class Block{
+​
+  constructor(data){
+    // ...
+    this.width = data[0];
+    this.length = data[1];
+    this.height = data[2];
+  }
+  
+  // ...
+  getWidth(){
+    return this.width;
+  }
+  getLength(){
+    return this.length;
+  }
+  getHeight(){
+    return this.height;
+  }
+  getVolume(){
+    return this.width * this.length * this.height;
+  }
+  getSurfaceArea(){
+    return 2 * ((this.width * this.length) + (this.height * this.length) + (this.width * this.height));
+  }
+}
