50d50879139735970d2a6d25447a3bdcc67c7d42
Modesto Amador <49541900+mofesdo@users.noreply.github.com>
2025-08-18 22:58:46 -0700

    Add solution - CodeHub

---
+++ 8-kyu/polish-alphabet/polish-alphabet.js
@@ -0,0 +1,8 @@
+function correctPolishLetters (string) {
+  const map = {
+    'ą':'a','ć':'c','ę':'e','ł':'l','ń':'n','ó':'o','ś':'s','ź':'z','ż':'z',
+    'Ą':'A','Ć':'C','Ę':'E','Ł':'L','Ń':'N','Ó':'O','Ś':'S','Ź':'Z','Ż':'Z'
+  };
+  return string.replace(/[ąćęłńóśźżĄĆĘŁŃÓŚŹŻ]/g, ch => map[ch]);
+​
+}
