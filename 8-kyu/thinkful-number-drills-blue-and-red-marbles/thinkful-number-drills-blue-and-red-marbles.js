fe1a026fed6622c24fd7b9b80f0cf9cfc3b561c1
Modesto Amador <49541900+mofesdo@users.noreply.github.com>
2025-08-27 22:15:36 -0700

    Add solution - CodeHub

---
+++ 8-kyu/thinkful-number-drills-blue-and-red-marbles/thinkful-number-drills-blue-and-red-marbles.js
@@ -0,0 +1,7 @@
+function guessBlue(blueStart, redStart, bluePulled, redPulled) {
+  const blueLeft = blueStart - bluePulled;
+  const redLeft = redStart - redPulled;
+  const totalLeft = blueLeft + redLeft;
+  
+  return blueLeft / totalLeft;
+}
