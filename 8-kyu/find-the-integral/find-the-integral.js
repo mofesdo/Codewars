bbce0d1c0be24f81fd01fb1501aaab611b238dfe
Modesto Amador <49541900+mofesdo@users.noreply.github.com>
2025-09-09 21:07:46 -0700

    Add solution - CodeHub

---
+++ 8-kyu/find-the-integral/find-the-integral.js
@@ -0,0 +1,6 @@
+function integrate(coefficient, exponent) {
+  let newExponent = exponent + 1;
+  let newCoefficient = coefficient / newExponent;
+  return `${newCoefficient}x^${newExponent}`;
+}
+​
