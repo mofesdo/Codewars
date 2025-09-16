b0a9f157643b33be6d565ca7b9032e8fc6bff916
Modesto Amador <49541900+mofesdo@users.noreply.github.com>
2025-09-15 21:08:13 -0700

    Add solution - CodeHub

---
+++ 8-kyu/alternating-case-<-equals->-alternating-case/alternating-case-<-equals->-alternating-case.js
@@ -0,0 +1,9 @@
+String.prototype.toAlternatingCase = function () {
+  return this.split('')
+    .map(char => char === char.toUpperCase()
+      ? char.toLowerCase()
+      : char.toUpperCase()
+    )
+    .join('');
+};
+​
