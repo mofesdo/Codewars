d7c82d835c47416fc95f33c1d1e43292ace47650
Modesto Amador <49541900+mofesdo@users.noreply.github.com>
2025-09-20 22:24:30 -0700

    Add solution - CodeHub

---
+++ 8-kyu/training-js-number-11-loop-statement-break-continue/training-js-number-11-loop-statement-break-continue.js
@@ -0,0 +1,14 @@
+function grabDoll(dolls) {
+  let bag = [];
+  for (let i = 0; i < dolls.length; i++) {
+    if (dolls[i] !== "Hello Kitty" && dolls[i] !== "Barbie doll") {
+      continue;
+    }
+    bag.push(dolls[i]);
+    if (bag.length === 3) {
+      break;
+    }
+  }
+  return bag;
+}
+​
