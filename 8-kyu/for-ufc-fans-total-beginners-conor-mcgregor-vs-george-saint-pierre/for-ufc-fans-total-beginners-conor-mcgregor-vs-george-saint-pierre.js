50bdae41d08b811d75430810ee5384fc21f4f9ca
Modesto Amador <49541900+mofesdo@users.noreply.github.com>
2025-10-21 22:15:38 -0700

    Add solution - CodeHub

---
+++ 8-kyu/for-ufc-fans-total-beginners-conor-mcgregor-vs-george-saint-pierre/for-ufc-fans-total-beginners-conor-mcgregor-vs-george-saint-pierre.js
@@ -0,0 +1,9 @@
+function quote(fighter) {
+  fighter = fighter.toLowerCase();
+​
+  if (fighter === 'george saint pierre')
+    return "I am not impressed by your performance.";
+  if (fighter === 'conor mcgregor')
+    return "I'd like to take this chance to apologize.. To absolutely NOBODY!";
+}
+​
