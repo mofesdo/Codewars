7a99bf733adbbf6c3d4e141ab695a9d75bf9ff14
Modesto Amador <49541900+mofesdo@users.noreply.github.com>
2025-11-10 23:45:20 -0800

    Add solution - CodeHub

---
+++ 7-kyu/all-star-code-challenge-number-22/all-star-code-challenge-number-22.js
@@ -0,0 +1,6 @@
+function toTime(seconds) {
+  const hours = Math.floor(seconds / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  return `${hours} hour(s) and ${minutes} minute(s)`;
+}
+​
