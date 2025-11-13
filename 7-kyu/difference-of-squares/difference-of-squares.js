c10a4de4bb09077b7269d47c68467947a8e5fa5e
Modesto Amador <49541900+mofesdo@users.noreply.github.com>
2025-11-12 22:10:15 -0800

    Add solution - CodeHub

---
+++ 7-kyu/difference-of-squares/difference-of-squares.js
@@ -0,0 +1,6 @@
+function differenceOfSquares(n) {
+const sum = n * (n + 1) / 2;
+  const squareOfSum = sum * sum;
+  const sumOfSquares = n * (n + 1) * (2 * n + 1) / 6;
+  return squareOfSum - sumOfSquares;
+}
