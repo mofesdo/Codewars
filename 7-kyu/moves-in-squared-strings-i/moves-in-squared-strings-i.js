bf01f58a8459c2c1411f8cb3b8d2e7a758d8bc30
Modesto Amador <49541900+mofesdo@users.noreply.github.com>
2025-11-19 22:17:23 -0800

    Add solution - CodeHub

---
+++ 7-kyu/moves-in-squared-strings-i/moves-in-squared-strings-i.js
@@ -0,0 +1,18 @@
+function vertMirror(s) {
+  return s
+    .split('\n')
+    .map(line => line.split('').reverse().join(''))
+    .join('\n');
+}
+​
+function horMirror(s) {
+  return s
+    .split('\n')
+    .reverse()
+    .join('\n');
+}
+​
+function oper(fct, s) {
+  return fct(s);
+}
+​
