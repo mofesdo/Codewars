a4d0c2c0f4c1a29962a96292abf3dbb9fb9c9dd0
Modesto Amador <49541900+mofesdo@users.noreply.github.com>
2025-12-26 23:51:03 -0800

    Add solution - CodeHub

---
+++ 7-kyu/nickname-generator/nickname-generator.js
@@ -0,0 +1,9 @@
+function nicknameGenerator(name) {
+  if (name.length < 4) {
+    return "Error: Name too short";
+  }
+​
+  const vowels = "aeiou";
+  return vowels.includes(name[2]) ? name.slice(0, 4) : name.slice(0, 3);
+}
+​
